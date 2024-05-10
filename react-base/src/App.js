5ee75d8af3a3c88c1e2615b4e0a49afd1dbbd2f3
Elandland <bodan99@gmail.com>
2024-05-10 16:23:30 +0900

    Feat. 5/10 2nd version Todo

--- react-base/src/App.js
+++ react-base/src/App.js
@@ -7,24 +7,41 @@ import BlinkCompnent from './components/BlinkComponent';
 import CBlinkComponent from './components/CBlinkComponent';
 import CountComponent from './components/CountComponent';
 import {useState} from "react";
+import ResisterInputButton from './components/ResisterInputButton';
+import FocusInputButton from './components/ResisterInputButton';
+import PrimeCalculator from './components/PrimeCalculator';
+import CountComponent2 from './components/CountComponent2';
+import Todo from './components/Todo';
+import NewTodo from './components/NewTodo';
+import NewNewTodo from './components/NewNewTodo';
 
-function App() {
-  const [showCount,setShowCount] = useState(true);
-  return (
-    <div className="App">
-      <button onClick={(e)=>{
-        setShowCount(!showCount);
-      }}>버튼</button>
-      {showCount ? <CountComponent/>: null}
-        {/* <HelloWorld/> */}
-        {/* <CaptionImage 
-        imgUrl = "https://wimg.mk.co.kr/news/cms/202312/02/20231202_01160106000003_L00.jpg"
-        caption="이건 트럭입니다."/> */}
-        {/* <BlinkCompnent text="깜빡"/> */}
-        {/* <CBlinkComponent text = "까암빡"/> */}
+// function App() {
+//   const [showCount,setShowCount] = useState(true);
+//   return (
+//     <div className="App">
+//       <button onClick={(e)=>{
+//         setShowCount(!showCount);
+//       }}>버튼</button>
+//       {showCount ? <CountComponent/>: null}
+//         {/* <HelloWorld/> */}
+//         {/* <CaptionImage 
+//         imgUrl = "https://wimg.mk.co.kr/news/cms/202312/02/20231202_01160106000003_L00.jpg"
+//         caption="이건 트럭입니다."/> */}
+//         {/* <BlinkCompnent text="깜빡"/> */}
+//         {/* <CBlinkComponent text = "까암빡"/> */}
 
+//     </div>
+//   );
+// }
+
+export default function App(){
+  return(
+    <div>
+      {/* <FocusInputButton/> */}
+      {/* <PrimeCalculator text={10}/> */}
+      {/* <CountComponent2/> */}
+      <NewTodo/>
+      {/* <NewNewTodo/> */}
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ react-base/src/components/NewTodo.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+import "./NewTodo.css";
+
+export default function Todo() {
+    const [text, setText] = useState("");
+    const [arr, setArr] = useState([])
+
+    const addArr = () => {
+        setArr((arr)=>[...arr,{text:text,color:boxcolor}]);
+    }
+
+    const [boxcolor, setBoxColor] = useState('white');
+
+    return (
+        <div>
+            <div className='title'>Todo App</div>
+            <div className='header'>
+                <div className='insert_div'>
+                    <input type="text" style={{backgroundColor:boxcolor}} onChange={e => {
+                        setText(e.target.value)
+                    }} placeholder='입력' />
+                    <button className="insert_button" onClick = {()=> { addArr();}}>입력</button>
+                </div>
+
+                <div >
+                    <button className="sel_col" onClick ={()=>{setBoxColor('antiquewhite');}} style={{backgroundColor:'antiquewhite'}} ></button>
+                    <button className="sel_col" onClick ={()=>{setBoxColor('blue');}}style={{backgroundColor:'blue'}}></button>
+                    <button className="sel_col" onClick ={()=>{setBoxColor('red');}}style={{backgroundColor:'red'}}></button>
+                    <button className="sel_col" onClick ={()=>{setBoxColor('white');}}style={{backgroundColor:'white'}}></button>
+                </div>
+            </div>
+            <h3>Todo Items</h3>
+            <div style={{display:'flex',flexDirection:'column',justifyContent:'center',margin:30,alignItems:'center'}}>
+                {arr.map((elem,idx) => <div key={idx} style={{backgroundColor:elem.color,width:600,height:30,margin:10} }>{elem.text}</div>)}
+            </div>
+        </div>
+
+    )
+}
